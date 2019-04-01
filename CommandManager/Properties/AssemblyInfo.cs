4bcd0a024995c6ce83e83857d4f1d2bc12bdb117
Rhatalin <danielflo1997@hotmail.com>
2019-04-01 15:22:59 +0200

    add first logo

--- CommandManager/Properties/AssemblyInfo.cs
+++ CommandManager/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CommandManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Command Manager")]
+[assembly: AssemblyDescription("GUI for Batch Files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Daniel Flockert")]
 [assembly: AssemblyProduct("CommandManager")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
